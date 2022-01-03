d367da7fead1d363658b74823fe57d00336b843d
Bagas Rizkiyanto <bagasrizkiyanto2@gmail.com>
2022-01-03 22:39:59 +0700

    revisi pertama

--- PenghitunganTest.java
+++ PenghitunganTest.java
@@ -18,10 +18,22 @@ public class PenghitunganTest {
         // TODO code application logic here\
         Penghitungan p = new Penghitungan();
         int bayar;
-
+        
+        System.out.println("Pengetesan Dimulai");
+        
+        System.out.println("Tes 1 :");
         bayar = p.ngitung(10, 10000);
         System.out.println(bayar);
-        p.dispose();
+        
+        System.out.println("Tes 2 : ");
+        bayar = p.ngitung(0, 10000);
+        System.out.println(bayar);
+        
+        System.out.println("Tes 3 : ");
+        bayar = p.ngitung(10, 0);
+        System.out.println(bayar);
+        
+        System.out.println("Pengetesan Selesai");
     }
 
 }
